acfa4d587815d0e4a00480323032cdda8688cb5d
Rahul Surabhi <rahul.surabhi09@gmail.com>
2017-03-02 13:01:25 +0530

    add a rating color functionality

--- src/helpers/helperFunctions.js
+++ src/helpers/helperFunctions.js
@@ -1,5 +1,13 @@
 const helper = {}
 
+helper.ratingColors = {
+  '1': 'red',
+  '2': 'red',
+  '3': 'blue',
+  '4': 'blue',
+  '5': 'green'
+}
+
 helper.generateActorsList = (movies) => {
   let nonUniqueActors = movies.reduce((acc, movie) => {
     return acc.concat(movie.actors)
@@ -12,6 +20,10 @@ helper.getMoviesBasedOnActor = (movies, actor) => {
   return movies.filter(movie => {
     return movie.actors.some(elem => elem === actor)
   })
+}
+
+helper.getRatingColor = (rating) => {
+  return helper.ratingColors[rating]
 }
 
 export default helper
