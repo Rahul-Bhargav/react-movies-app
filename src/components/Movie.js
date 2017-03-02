b30162a1a1ef5a7718bf41e27c78e1646c0af7d2
Rahul Surabhi <rahul.surabhi09@gmail.com>
2017-03-02 11:59:53 +0530

    Update Movie Component with style

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import '../styles/movie.css'
 
 const Movie = (props) => {
+  const themeStyle = {
+    backgroundColor: props.movie.theme,
+    display: 'inline-block',
+    height: '80px',
+    flexBasis: '10%'
+  }
   return (
     <div className="movie">
+      <div style={themeStyle} />
       <div> {props.movie.name} </div>
       <div className="description">
         {props.movie.description}
